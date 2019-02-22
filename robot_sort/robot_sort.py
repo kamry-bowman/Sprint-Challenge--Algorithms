ea1e9bc0d82e65be01633dc0f5b53c0f3fb94d88
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-22 12:23:17 -0700

    fix so it exits properly

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -127,7 +127,7 @@ class SortingRobot:
         else:
             self.sort()
 
-    def helper(sort):
+    def bubble(self):
         # check if held item is greater than item in front
         if self.compare_item() == 1:
             self.swap_item()
@@ -148,27 +148,21 @@ class SortingRobot:
         # exit condition
         if self.can_move_right():
             self.move_right()
-            return self.sort()
+            return self.bubble()
 
         elif self.light_is_on():
             self.set_light_off()
-            # self.swap_item()
+            self.swap_item()
             return self.return_to_start()
+
+        else:
+            self.swap_item()
 
     def sort(self):
         self.swap_item()
-
-        else:
-            # the list is sorted, so this is the final move
-            print(self._item, self.is_empty(), self._position)
-            if self.compare_item() == 1:
-                self.move_left()
-                self.swap_item()
-            else:
-                self.move_left()
-                self.swap_item()
-                self.move_right()
-
+        self.move_right()
+        
+        self.bubble()
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
@@ -177,9 +171,9 @@ if __name__ == "__main__":
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
          45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-    l2 = [15, 41, 58, 49, 26, 4, 15, 17]
+    l2 = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 2, 9, 66, 1]
 
-    robot = SortingRobot(l)
+    robot = SortingRobot(l2)
 
     robot.sort()
     print(robot._list)
